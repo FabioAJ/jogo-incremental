dc45e7996b211aadaa6bc9309bc939d54ff262c4
FabioAJ <fabioaugusto.aj@gmail.com>
2018-10-18 17:55:28 -0300

    cronometro

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 var pontos = 0;
+var crono = 0;
 
 function aumentoDePontos() {
+    if (pontos == 0) {
+        setInterval(cronometro, 1000);
+    }
     pontos += 1;
     document.getElementById("demo").innerHTML = "Pontos: " + pontos;
+    
+}
+
+function cronometro() {
+    crono += 1;
+    document.getElementById("cronom").innerHTML = "Tempo: " + crono;
+}
+
+function scrollWin() {
+    window.scrollTo(0, 500);
 }
