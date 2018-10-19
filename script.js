b9bec7e76a45a3c7f1a26839d13c3650119db4bc
FabioAJ <fabioaugusto.aj@gmail.com>
2018-10-19 17:57:03 -0300

    começando melhorias e funções

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var pontos = 0;
 var crono = 0;
+var melhorias = [false, false, false];
 
 function aumentoDePontos() {
     if (pontos == 0) {
@@ -7,14 +8,45 @@ function aumentoDePontos() {
     }
     pontos += 1;
     document.getElementById("demo").innerHTML = "Pontos: " + pontos;
-    
+
 }
 
 function cronometro() {
     crono += 1;
     document.getElementById("cronom").innerHTML = "Tempo: " + crono;
+    console.log(crono);
 }
 
-function scrollWin() {
-    window.scrollTo(0, 500);
+function scrollUpgrades() {
+    window.scrollTo(0, 700);
+}
+
+function scrollBolaDeLa() {
+    window.scrollTo(0, -700);
+}
+
+function melhoria1(){
+    pontos += 1;
+    document.getElementById("demo").innerHTML = "Pontos: " + pontos;
+}
+
+function melhoria2(){
+    if (pontos >= 250 && melhorias[1] === false){
+
+    }
+
+}
+
+function melhoria3(){
+    if (pontos >= 500 && melhorias[2] === false){
+
+    }
+
+}
+
+function verificacao(){
+    if (pontos >= 20 && melhorias[0] === false){
+            melhorias[0] = true;
+            setInterval(melhoria1, 20000);
+    }
 }
